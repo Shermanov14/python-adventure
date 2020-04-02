b46cd837586f0c849b253d774aa097ab7feecba1
Shermanov14 <50887789+Shermanov14@users.noreply.github.com>
2020-04-01 23:47:00 -0400

    Update game.py

--- game.py
+++ game.py
@@ -6,11 +6,8 @@
 ####################################
 
 player_name = ""
-player_health = 100
-player_money = 50
+player_level = 1
 player_species = "Human"
-player_weapons = []
-player_items = []
 
 ####################################
 #            Game Start!           #
@@ -21,35 +18,55 @@ player_items = []
 print()
 print()
 print("   ####################################################")
-print("   # Hello! This is a choose-your-own-adventure game. #")
-print("   #  You are a hero trying to save your kingdom of   #")
-print("   #       Kanbal from the evil King Rogsam.          #")
+print("   # Hello! This is a choose-your-own-adventure race. #")
+print("   #  Make right decision with turns on the race track#")
+print("   #       and be first at the finishline!            #")
+print("   #             BEST OF LUCK SPEEEDY                 #")
 print("   ####################################################")
 print()
 print()
 
 while(player_name == ""):
-    player_name = input("What is your name, Hero? : ")
+    player_name = input("What is your name, Racer? : ")
 
 print()
-print("Hi, " + player_name + "! You are our hero and savior! Please save the Kanbalese mountain people from the evil King Rogsam.")
-print("You start off with no weapons or items, but if you make the right choices, you might survive.")
+print("Welcome, " + player_name + "! You are about to start adventurous race with World Top Racers ")
+print("You start off with slowest race car Honda Civic)) and depending on your decisions the vehicle model and speed will upgrade or downgrade")
 print()
 
-#    ---Section Author: <your name>---   #
-print("You pass by a traveler who looks injured by the woods. What do you do?")
-print("1. Ignore him")
-print("2. Try to help him")
-decision = input("Pick a number: ")
+#    ---Section Author: <Akhror>---   #
+print("The race started and you are the last, make following choices ...")
+print("1. Hit the speed all they way to the floor")
+print("2. Let the car to warm up a bit before speeding")
+decision = input("Pick your choice: ")
 print()
 if (decision == "1"):
-    print("You try to leave, but trip over a rock and hit your head!")
-    player_health = player_health - 10
-    print("Your health now: ")
-    print(player_health)
+    print("You car is overheated, you`re out of race")
+    player_level = player_level - 0
 elif (decision == "2"):
-    print("He appreciates that you helped him, and he gives you a potion! A potion heals 20 health points.")
-    player_items.append("Potion")
-    print("Your items now: ")
-    print(player_items)
+    print("Good work, the car is upgraded to Subaru race car")
+    player_level = player_level - 10
+print("Your driving skills are average now, suddenly your coach called you to pit stop ...")
+print("1. Went to pit stop as soon as possible")
+print("2. Ignore the message and keep going ")
+decision = input("What would you do?: ")
+print()
+if (decision == "1"):
+    print("Nice, you got new BMW ")
+    player_level = player_level - 50
+elif (decision == "2"):
+    print("Quickly ran out of the fuel and stopped at middle of the race ")
+    player_level = player_level - 5
+print("The flag went up for last circle and your alarm started ringing zzz zzz ...")
+print("1. Let the alarm go to snooze and keep racing")
+print("2. Woke up and get ready to work")
+decision = input("What is your action?: ")
+print()
+if (decision == "1"):
+    print("Congrats, you are late for work again")
+    player_level = player_level - 33
+elif (decision == "2"):
+    print("Congrats, you made another 5$ towars your real race car")
+    player_level = player_level - 99
 #            ---section end---           #
+
